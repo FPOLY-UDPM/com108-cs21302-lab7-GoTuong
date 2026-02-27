9233b8f673c1f0de5e2c291e5d39fe6acec320f7
GoTuong <tuongtung2018@gmail.com>
2026-02-28 00:10:01 +0700

    nộp bài

--- bai2.c
+++ bai2.c
@@ -1,12 +1,41 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
+ * Họ và tên: [Lê Minh Tường]
+ * MSSV :      [PS37909]
  * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
  *****************************************************************************/
 
-//  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
-//  Input: Nhập vào username và password 
+//  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD
+//  Input: Nhập vào username và password
 //  Output: Đăng nhập thành công hay không thành công
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char username[100];
+    char password[100];
+
+    char correctUser[] = "admin";
+    char correctPass[] = "123456";
+
+    printf("Nhap username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = 0;
+
+    printf("Nhap password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strcspn(password, "\n")] = 0;
+
+    if (strcmp(username, correctUser) == 0 && strcmp(password, correctPass) == 0)
+    {
+        printf("Dang nhap thanh cong\n");
+    }
+    else
+    {
+        printf("Dang nhap khong thanh cong\n");
+    }
+
+    return 0;
+}
